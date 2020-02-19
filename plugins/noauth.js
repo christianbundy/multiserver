fb330132e5fd1d1338454c85bffcb4f28f747da4
Anders Rune Jensen <arj03@protonmail.ch>
2020-02-19 21:29:55 +0100

    Assign a correct remote id instead of our own id

--- plugins/noauth.js
+++ plugins/noauth.js
@@ -1,18 +1,25 @@
 module.exports = function (opts) {
+  var remote = ''
+
   return {
     name: 'noauth',
     create: function (_opts) {
       return function (stream, cb) {
         cb(null, {
-          remote: opts.keys.publicKey,
+          remote: new Buffer(remote, 'base64'),
           auth: { allow: null, deny: null },
           source: stream.source,
           sink: stream.sink,
-          address: 'noauth:' + opts.keys.publicKey.toString('base64')
+          address: 'noauth:' + remote
         })
       }
     },
     parse: function (str) {
+      var ary = str.split(':')
+      if(ary[0] !== 'noauth') return null
+
+      remote = ary[1].substr(0, ary[1].length-8)
+
       return {}
     },
     stringify: function () {
